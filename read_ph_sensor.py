6e9118ce0bca696c513b99022d80b795d31859f0
Brett Longworth <blongworth@whoi.edu>
2024-08-06 22:03:26 +0000

    add db logging

--- read_ph_sensor.py
+++ read_ph_sensor.py
@@ -5,6 +5,7 @@ import csv
 from datetime import datetime
 import os
 import re
+import sqlite3
 
 def read_instrument(port, baudrate, timeout=2):
     with serial.Serial(port, baudrate, timeout=timeout) as ser:
@@ -44,6 +45,12 @@ def parse_data(data):
     else:
         raise ValueError("Invalid data format")
 
+def log_data_db(data):
+    var_string = ', '.join('?' * len(data))
+    query_string = f"INSERT INTO ph VALUES ({var_string});"
+    c.execute(query_string, data)
+    conn.commit()
+
 def log_data(filename, data):
     file_exists = os.path.isfile(filename)
     
@@ -65,6 +72,7 @@ def scheduled_reading(scheduler, port, baudrate, filename):
         if raw_data:
             parsed_data = parse_data(raw_data)
             log_data(filename, parsed_data)
+            log_data_db(parsed_data)
             print("Logged data:", parsed_data)
         else:
             print(f"Time: {datetime.now().strftime('%m/%d/%Y %H:%M:%S')}")
@@ -85,6 +93,20 @@ if __name__ == "__main__":
     BAUDRATE = 115200  
     LOGFILE = 'instrument_data.csv'  # Name of the CSV file
     
+
+    # Connect to the SQLite database 
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+
+    # Create a table to store the data
+    # or append if it already exists
+    c.execute('''CREATE TABLE IF NOT EXISTS ph
+            (samp_num INTEGER, ph_timestamp TEXT, 
+             v_bat REAL, v_bias_pos REAL, v_bias_neg REAL, 
+             t_board REAL, h_board REAL, vrse REAL, vrse_std REAL, 
+             cevk REAL, cevk_std REAL, ce_ik REAL, i_sub REAL
+             )''')
+
     s = sched.scheduler(time.time, time.sleep)
     
     # schedule first reading immediately
@@ -95,4 +117,5 @@ if __name__ == "__main__":
     try:
         s.run()
     except KeyboardInterrupt:
+        conn.close()
         print("\nScheduled readings stopped.")
