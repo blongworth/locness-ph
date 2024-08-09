b46793b1fdf8e9ab877f9cbe4cfd6b1ba74e2579
Brett Longworth <blongworth@whoi.edu>
2024-08-09 16:10:15 -0400

    add cal pH

--- read_ph_sensor.py
+++ read_ph_sensor.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import os
 import re
 import sqlite3
+from calc_pH_DeepSeapHOx import calc_pH
 
 READ_TIME = 10 # time between readings
 PORT = 'COM6'  # Adjust this to your serial port
@@ -15,6 +16,12 @@ BAUDRATE = 115200
 # DB_PATH = 'C:/Users/CSL 2/Documents/LOCNESS_data/data.db' # Path to SQLite DB
 LOGFILE = 'pH_data.csv'  # Name of the CSV file
 DB_PATH = 'data.db' # Path to SQLite DB
+
+# Fixed values for rough pH calibration
+TEMP = 25
+SAL = 30
+K0 = -1.42765
+K2 = -0.001037
     
 def read_instrument(port, baudrate, timeout=2):
     with serial.Serial(port, baudrate, timeout=timeout) as ser:
@@ -44,9 +51,7 @@ def read_instrument(port, baudrate, timeout=2):
             time.sleep(0.1)
         return 0
         
-        
-
-def parse_data(data):
+def parse_data(data, temp, sal, k0, k2):
     # Use regex to split the data, handling variable whitespace
     pattern = r'#(\d+)\s+(\d{2}/\d{2}/\d{4})\s+(\d{2}:\d{2}:\d{2})\s+([-\d.]+)\s+([-\d.]+)\s+([-\d.]+)\s+([-\d.]+)\s+(\d+)\s+([-\d.]+)\s+([-\d.]+)\s+([-\d.]+)\s+([-\d.]+)\s+([-\d.]+)\s+([-\d.]+)'
     match = re.match(pattern, data)
@@ -55,10 +60,13 @@ def parse_data(data):
         samp_num = int(match.group(1))
         datetime_str = f"{match.group(2)} {match.group(3)}"
         values = [float(match.group(i)) for i in range(4, 15)]
-        
-        return [samp_num, datetime_str] + values
+        timestamp = time.time()
+        ts = datetime.fromtimestamp(timestamp)
+        ph_free, ph_tot = calc_pH(values[8], 0, temp, sal, k0, k2)
+        return [ts, samp_num, datetime_str] + values + [temp, sal, k0, k2, ph_free, ph_tot]
     else:
         raise ValueError("Invalid data format")
+
 
 def log_data_db(data):
     var_string = ', '.join('?' * len(data))
@@ -74,9 +82,9 @@ def log_data(filename, data):
         
         if not file_exists:
             # Write header if file doesn't exist
-            csvwriter.writerow(['samp_num', 'datetime', 'v_bat', 'v_bias_pos', 'v_bias_neg', 
+            csvwriter.writerow(['pc_time', 'samp_num', 'ph_time', 'v_bat', 'v_bias_pos', 'v_bias_neg', 
                                 't_board', 'h_board', 'vrse', 'vrse_std', 'cevk', 'cevk_std', 
-                                'ce_ik', 'i_sub'])
+                                'ce_ik', 'i_sub', 'cal_temp', 'cal_sal', 'k0', 'k2', 'pH_free', 'ph_total'])
         
         csvwriter.writerow(data)
 
@@ -85,7 +93,7 @@ def scheduled_reading(scheduler, port, baudrate, filename):
         raw_data = read_instrument(port, baudrate)
         
         if raw_data:
-            parsed_data = parse_data(raw_data)
+            parsed_data = parse_data(raw_data, TEMP, SAL, K0, K2)
             log_data(filename, parsed_data)
             log_data_db(parsed_data)
             print("Logged data:", parsed_data)
@@ -114,7 +122,9 @@ if __name__ == "__main__":
             (samp_num INTEGER, ph_timestamp TEXT, 
              v_bat REAL, v_bias_pos REAL, v_bias_neg REAL, 
              t_board REAL, h_board REAL, vrse REAL, vrse_std REAL, 
-             cevk REAL, cevk_std REAL, ce_ik REAL, i_sub REAL
+             cevk REAL, cevk_std REAL, ce_ik REAL, i_sub REAL,
+             cal_temp REAL, cal_sal REAL, k0 REAL, k2 REAL,
+             pH_free REAL, ph_total REAL
              )''')
 
     s = sched.scheduler(time.time, time.sleep)
