b0538a41f2cd180893bbdad04fb0dd92518bdb78
Brett Longworth <blongworth@whoi.edu>
2024-08-15 13:48:56 -0400

    add dependencies and config via files

--- read_ph_sensor.py
+++ read_ph_sensor.py
@@ -7,22 +7,8 @@ import os
 import re
 import sqlite3
 from calc_pH_DeepSeapHOx import calc_pH
-
-READ_TIME = 10  # time between readings
-PORT = "COM6"  # Adjust this to your serial port
-# PORT = '/dev/tty.usbserial-FT9439MT0'  # Adjust this to your serial port
-BAUDRATE = 115200
-# LOGFILE = 'C:/Users/CSL 2/Documents/LOCNESS_data/pH_data.csv'  # Name of the CSV file
-# DB_PATH = 'C:/Users/CSL 2/Documents/LOCNESS_data/data.db' # Path to SQLite DB
-LOGFILE = "pH_data.csv"  # Name of the CSV file
-DB_PATH = "data.db"  # Path to SQLite DB
-
-# Fixed values for rough pH calibration
-TEMP = 25
-SAL = 30
-K0 = -1.42765
-K2 = -0.001037
-
+from default_config import *
+from config import *
 
 def read_instrument(port, baudrate, timeout=2):
     with serial.Serial(port, baudrate, timeout=timeout) as ser:
